93a706eadf9c9ec84434e649e70cb10fdfa56012
Ayodele Fagbami <afagbami@gmail.com>
2022-07-21 10:40:41 +0100

    set up consumer

--- src/main/java/com/example/activemqtut/listener/Consumer.java
+++ src/main/java/com/example/activemqtut/listener/Consumer.java
@@ -1,4 +1,12 @@
 package com.example.activemqtut.listener;
 
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Consumer {
+    @JmsListener(destination = "standalone.queue")
+    public void consume(String message) {
+        System.out.println("Received Message: " + message);
+    }
 }
