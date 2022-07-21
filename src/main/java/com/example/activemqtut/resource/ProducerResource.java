876b6b10b10ea454821aa8e8c4122c51ca783a6b
Ayodele Fagbami <afagbami@gmail.com>
2022-07-21 10:12:39 +0100

    created rest controller and added get mapping

--- src/main/java/com/example/activemqtut/resource/ProducerResource.java
+++ src/main/java/com/example/activemqtut/resource/ProducerResource.java
@@ -1,4 +1,18 @@
 package com.example.activemqtut.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/publish")
 public class ProducerResource {
+
+    @GetMapping("/{message")
+    public String publish(@PathVariable("message") final String message) {
+
+        
+
+    }
 }
