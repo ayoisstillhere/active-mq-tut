5a2a707b9491cb1720a4282f17c9813db3667f4a
Ayodele Fagbami <afagbami@gmail.com>
2022-07-21 09:55:15 +0100

    initial commit

---
+++ src/main/java/com/example/activemqtut/ActiveMqTutApplication.java
@@ -0,0 +1,13 @@
+package com.example.activemqtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMqTutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/activemqtut/ActiveMqTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activemqtut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
